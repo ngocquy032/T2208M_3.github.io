0450c22f40df85c00fd9f7161cf4cf997ca73abb
ngocquy032 <112050702+ngocquy032@users.noreply.github.com>
2023-01-10 10:18:56 +0700

    update fix

--- javascript/customer-support.js
+++ javascript/customer-support.js
@@ -45,10 +45,27 @@ function validateMail(mail){
   return REGEX_MAIL.test(mail);
 }
 
+// check phone
+let phoneInput = document.getElementById('phone');
+let phoneMsgInput = document.getElementById('checkPhone');
+const REGEX_PHONE= /(((\+|)84)|0)(3|5|7|8|9)+([0-9]{8})\b/;
+
+function checkPhone(){
+  let numberValue = phoneInput.value;
+  let checkPhone = validatePhoneNumber(numberValue);
+  if(!checkPhone){
+      phoneMsgInput.textContent = 'Enter the wrong number format, please try again';
+  }else{
+      phoneMsgInput.textContent ='';
+  }
+}
+function validatePhoneNumber(phoneNumber) {
+  return REGEX_PHONE.test(phoneNumber);
+}
 
 function onClickBtnSend(){
-  console.log('mailMsgInput.textContent: ', !mailMsgInput.textContent);
-  if(mailInput.value != "" && textareaInput.value != "" && nameInput.value != "" ){
+  // console.log('mailMsgInput.textContent: ', !mailMsgInput.textContent);
+  if(mailInput.value != "" && textareaInput.value != "" && nameInput.value !="" && phoneInput.value !=""){
   alert('send support successfully')
 
   }
